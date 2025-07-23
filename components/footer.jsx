8f8e870e8effcc74ce41b28c888e68f1eb4d0e40
Marketcalls <rajandran@marketcalls.in>
2025-07-23 20:16:39 +0530

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -32,7 +32,7 @@ export function Footer() {
           <span className="hidden md:inline">•</span>
           <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
             <p className="text-sm text-muted-foreground">
-              v1.0.0.26
+              v1.0.0.27
             </p>
           </div>
         </div>
